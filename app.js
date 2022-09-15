1c19f845475d199ffe3ad38ec8c519c189862df2
Muhammed Apak <bufman25@gmail.com>
2022-09-15 21:55:30 +0300

    Input checked

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// alert("hello")
+const container = document.querySelector("#container");
+const input = document.getElementById("input")
+
+calculator.addEventListener("click", clickBox)
+
+function clickBox(e){
+    let clickedButton = e.target.textContent;
+   
+    input.value = clickedButton;
+     if(input.value){
+        
+     }
+  
+}
